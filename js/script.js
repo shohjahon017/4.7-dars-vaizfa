452df19d7008c5369872b06d5892e0fa60f8e196
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-07-28 19:27:03 +0500

    .

---
+++ js/script.js
@@ -0,0 +1,66 @@
+//1-masala
+// function squareElement(arr) {
+//   arr = Math.sqrt(4);
+//   return arr;
+// }
+// console.log(squareElement());
+
+//2-masala
+
+//Massivlarga oid masalalar
+//1-masala
+// function sumElement(arr) {
+//   let sum = 0;
+//   for (const iterator of arr) {
+//     sum += iterator;
+//   }
+//   return sum;
+// }
+// console.log(sumElement([1, 5, 3, 6, 8, 9]));
+
+//2-masala
+// let arr = [5, 9, 8, 3, 4, 6];
+// function maxElement(arr) {
+//   let max = arr[0];
+//   for (const iterator of arr) {
+//     if (iterator > max) {
+//       max = iterator;
+//     }
+//   }
+//   return max;
+// }
+// console.log(maxElement(arr));
+
+//3-masala
+// function reverseElement(arr) {
+//   let res = arr.reverse();
+//   return res;
+// }
+// console.log(reverseElement([5, 2, 3, 6, 9]));
+
+//4-masala
+// let arr = [-2, 6, -9, -3, 4, -8];
+// function filterElement(arr) {
+//   let odd = [];
+//   let res = arr.filter((element) => {
+//     if (element > 0) {
+//       odd.push(element);
+//     }
+//   });
+//   return odd;
+// }
+// console.log(filterElement(arr));
+
+//5-masala
+// let arr = [2, 6, 3, 5, 4];
+// function squareElement(arr) {
+//   let res = [];
+//   for (const iterator of arr) {
+//     res.push(iterator ** 2);
+//   }
+//   return res;
+// }
+// console.log(squareElement(arr));
+
+//6-masala
+let arr = [5, 9, 3, 5, 7, 6, 2];
